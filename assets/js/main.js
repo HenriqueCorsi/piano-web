2e328dcfddd9c9ebd824fcba0b3bfe09e5e71182
Paulo Henrique <ph55@hotmail.com>
2023-02-03 17:31:24 -0300

    .

---
+++ assets/js/main.js
@@ -0,0 +1,95 @@
+ function key01(){
+    document.getElementById('key01').play();
+ }
+
+ function key02(){
+    document.getElementById('key02').play();
+ }
+
+ function key03(){
+    document.getElementById('key03').play();
+ }
+
+ function key04(){
+    document.getElementById('key04').play();
+ }
+
+ function key05(){
+    document.getElementById('key05').play();
+ }
+
+ function key06(){
+    document.getElementById('key06').play();
+ }
+
+ function key07(){
+    document.getElementById('key07').play();
+ }
+
+ function key08(){
+    document.getElementById('key08').play();
+ }
+
+ function key09(){
+    document.getElementById('key09').play();
+ }
+
+ function key10(){
+    document.getElementById('key10').play();
+ }
+
+ function key11(){
+    document.getElementById('key11').play();
+ }
+
+ function key12(){
+    document.getElementById('key12').play();
+ }
+
+ function key13(){
+    document.getElementById('key13').play();
+ }
+
+ function key14(){
+    document.getElementById('key14').play();
+ }
+
+ function key15(){
+    document.getElementById('key15').play();
+ }
+
+ function key16(){
+    document.getElementById('key16').play();
+ }
+
+ function key17(){
+    document.getElementById('key17').play();
+ }
+
+ function key18(){
+    document.getElementById('key18').play();
+ }
+
+ function key19(){
+    document.getElementById('key19').play();
+ }
+
+ function key20(){
+    document.getElementById('key20').play();
+ }
+
+ function key21(){
+    document.getElementById('key21').play();
+ }
+
+ function key22(){
+    document.getElementById('key22').play();
+ }
+
+ function key23(){
+    document.getElementById('key23').play();
+ }
+
+ function key24(){
+    document.getElementById('key24').play();
+ }
